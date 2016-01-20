93649cf2300ba019443b595bd10facb9c733d9cf
David Chen <davidchen1911@gmail.com>
2016-01-19 22:31:06 -0800

    remove test case for GetRestaurantsNearby

--- server/src/db/DBConnectionDBUnit.java
+++ server/src/db/DBConnectionDBUnit.java
@@ -92,25 +92,4 @@ public class DBConnectionDBUnit extends DatabaseTestCase{
         assertEquals(expected, recommend);
 		dbconn.close();
 	}
-
-	@Test
-	public void testGetRestaurantsNearLoation() throws Exception {
-		DBConnection dbconn = new DBConnection(
-				"jdbc:mysql://localhost:3306/unittest?user=root&password=root");
-		JSONArray restaurants = dbconn.GetRestaurantsNearLoation("1111", 30.0, 20.0);
-		Set<String> nearby_businessids = new HashSet<>();
-        int length = restaurants.length();
-        for(int i = 0; i < length; i++){
-            JSONObject restaurant = restaurants.getJSONObject(i);
-            nearby_businessids.add(restaurant.getString("business_id"));  
-        }  
-        Set<String> expected = new HashSet<String>(Arrays.asList(
-        		"--jFTZmywe7StuZ2hEjxyA", "--pOlFxITWnhzc7SHSIP0A", 
-        		"--BlvDO_RG2yElKu9XA1_g", "--Y_2lDOtVDioX5bwF6GIw", 
-        		"--zgHBiQpr8H2ZqSdGmguQ", "--qeSYxyn62mMjWvznNTdg", 
-        		"-024YEtnIsPQCrMSHCKLQw", "--5jkZ3-nUPZxUvtcbr8Uw", 
-        		"-0bl9EllYlei__4dl1W00Q", "--UE_y6auTgq3FXlvUMkbw"));
-        assertEquals(expected, nearby_businessids);
-		dbconn.close();
-	}
 }
