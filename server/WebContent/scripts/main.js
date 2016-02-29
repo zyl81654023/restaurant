ed2d0d282804ac0240f7e7f0fd54ef10923f0646
Su <ysu@box.com>
2016-02-29 10:04:17 -0800

    Code Refactor.

--- server/WebContent/scripts/main.js
+++ server/WebContent/scripts/main.js
@@ -1,12 +1,15 @@
 /**
- * Triggered when the whole page is loaded
+ * Variables
  */
-
-var lng = -122.08;
-var lat = 37.38;
-var user_id = '1111';
 var projectname = '/Dashi';
+var user_id     = '1111';
+var lng         = -122.08;
+var lat         = 37.38;
 
+/**
+ * Triggered when the whole page is loaded
+ * here we locate our location
+ */
 function onPageLoaded() {
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(onPositionUpdated, onLoadPositionFailed, {maximumAge: 60000});
@@ -28,85 +31,26 @@ function onLoadPositionFailed() {
   loadNearbyRestaurants();
 }
 
-/**
- * Load the nearby restaurants
- */
-function loadNearbyRestaurants() {
-  activeBtn('nearby-btn');
-
-  // The request parameters
-  var url = projectname + '/restaurants';
-  var params = 'user_id=' + user_id + '&lat=' + lat + '&lon=' + lng;
-  var req = JSON.stringify({});
-
-  showLoadingMessage('Loading nearby restaurants...');
-  
-  ajax('get', url + '?' + params, req, 
-	function (res) {
-      var restaurants = JSON.parse(res);
-      if (!restaurants || restaurants.length === 0) {
-    	showWarningMessage('No nearby restaurant.');
-      } else {
-        listRestaurants(restaurants);
-      }
-    },
-    function () {
-      showErrorMessage('Cannot load nearby restaurants.');
-    }  
-  );
-}
-
-function loadFavoriteRestaurants() {
-  activeBtn('fav-btn');
-
-  // The request parameters
-  var url = projectname + '/history';
-  var params = 'user_id=' + user_id;
-  var req = JSON.stringify({});
-  
-  showLoadingMessage('Loading favorite restaurants...');
-
-  ajax('get', url + '?' + params, req, 
-    function (res) {
-      var restaurants = JSON.parse(res);
-      if (!restaurants || restaurants.length === 0) {
-    	showWarningMessage('No favorite restaurant.');
-      } else {
-    	listRestaurants(restaurants);
-      }
-    },
-    function () {
-      showErrorMessage('Cannot load favorite restaurants.');
-    }  
-  );
-}
+// -----------------------------------
+//  Helper Functions
+// -----------------------------------
 
 /**
- * Load favorite restaurants
+ * A helper function that makes a navigation button active
+ * 
+ * @param btnId - The id of the navigation button
  */
-function loadRecommendedRestaurants() {
-  activeBtn('recommend-btn');
-
-  // The request parameters
-  var url = projectname + '/recommendation';
-  var params = 'user_id=' + user_id;
-  var req = JSON.stringify({});
+function activeBtn(btnId) {
+  var btns = document.getElementsByClassName('main-nav-btn');
   
-  showLoadingMessage('Loading recommended restaurants...');
-
-  ajax('get', url + '?' + params, req, 
-    function (res) {
-      var restaurants = JSON.parse(res);
-      if (!restaurants || restaurants.length === 0) {
-    	showWarningMessage('No recommended restaurant. Make sure you have favorites.');
-      } else {
-    	listRestaurants(restaurants);
-      }
-    },
-    function () {
-      showErrorMessage('Cannot load recommended restaurants.');
-    } 
-  );
+  // deactivate all navigation buttons
+  for (var i = 0; i < btns.length; i++) {
+    btns[i].className = btns[i].className.replace(/\bactive\b/, '');
+  }
+  
+  // active the one that has id = btnId
+  var btn = document.getElementById(btnId);
+  btn.className += ' active';
 }
 
 function showLoadingMessage(msg) {
@@ -125,18 +69,169 @@ function showErrorMessage(msg) {
 }
 
 /**
- * List restaurants
+ * A helper function that creates a DOM element <tag options...>
+ * 
+ * @param tag
+ * @param options
+ * @returns
  */
-function listRestaurants(restaurants) {
-  // Clear the current results
-  var restaurantList = document.getElementsByClassName('restaurant-list')[0];
-  restaurantList.innerHTML = '';
+function $(tag, options) {
+  var element = document.createElement(tag);
 
-  for (var i = 0; i < restaurants.length; i++) {
-    addRestaurant(restaurantList, restaurants[i]);
+  for (var option in options) {
+    if (options.hasOwnProperty(option)) {
+      element[option] = options[option];
+    }
+  }
+
+  return element;
+}
+
+/**
+ * AJAX helper
+ * 
+ * @param method - GET|POST|PUT|DELETE
+ * @param url - API end point
+ * @param callback - This the successful callback
+ * @param errorHandler - This is the failed callback
+ */
+function ajax(method, url, data, callback, errorHandler) {
+  var xhr = new XMLHttpRequest();
+
+  xhr.open(method, url, true);
+
+  xhr.onload = function () {
+    if (xhr.status == 200) {
+      callback(xhr.responseText);
+    }
+  };
+
+  xhr.onerror = function () {
+    console.error("The request couldn't be completed.");
+    errorHandler();
+  };
+
+  if (data === null) {
+    xhr.send();
+  } else {
+    xhr.setRequestHeader("Content-Type", "application/json;charset=utf-8");
+    xhr.send(data);
   }
 }
 
+// -------------------------------------
+//  AJAX call server-side APIs
+// -------------------------------------
+
+/**
+ * API #1
+ * Load the nearby restaurants
+ * API end point: [GET] /Dashi/restaurants?user_id=1111&lat=37.38&lon=-122.08
+ */
+function loadNearbyRestaurants() {
+  activeBtn('nearby-btn');
+
+  // The request parameters
+  var url = projectname + '/restaurants';
+  var params = 'user_id=' + user_id + '&lat=' + lat + '&lon=' + lng;
+  var req = JSON.stringify({});
+  
+  // display loading message
+  showLoadingMessage('Loading nearby restaurants...');
+  
+  // make AJAX call
+  ajax('get', url + '?' + params, req, 
+	// successful callback
+	function (res) {
+      var restaurants = JSON.parse(res);
+      if (!restaurants || restaurants.length === 0) {
+    	showWarningMessage('No nearby restaurant.');
+      } else {
+        listRestaurants(restaurants);
+      }
+    },
+    // failed callback
+    function () {
+      showErrorMessage('Cannot load nearby restaurants.');
+    }  
+  );
+}
+
+/**
+ * API #2
+ * Load favorite (or visited) restaurants
+ * API end point: [GET] /Dashi/history?user_id=1111
+ */
+function loadFavoriteRestaurants() {
+  activeBtn('fav-btn');
+
+  // The request parameters
+  var url = projectname + '/history';
+  var params = 'user_id=' + user_id;
+  var req = JSON.stringify({});
+  
+  // display loading message
+  showLoadingMessage('Loading favorite restaurants...');
+
+  // make AJAX call
+  ajax('get', url + '?' + params, req, 
+    function (res) {
+      var restaurants = JSON.parse(res);
+      if (!restaurants || restaurants.length === 0) {
+    	showWarningMessage('No favorite restaurant.');
+      } else {
+    	listRestaurants(restaurants);
+      }
+    },
+    function () {
+      showErrorMessage('Cannot load favorite restaurants.');
+    }  
+  );
+}
+
+/**
+ * API #3
+ * Load recommended restaurants
+ * API end point: [GET] /Dashi/recommendation?user_id=1111
+ */
+function loadRecommendedRestaurants() {
+  activeBtn('recommend-btn');
+
+  // The request parameters
+  var url = projectname + '/recommendation';
+  var params = 'user_id=' + user_id;
+  var req = JSON.stringify({});
+  
+  // display loading message
+  showLoadingMessage('Loading recommended restaurants...');
+
+  // make AJAX call
+  ajax('get', url + '?' + params, req,
+	// successful callback
+    function (res) {
+      var restaurants = JSON.parse(res);
+      if (!restaurants || restaurants.length === 0) {
+    	showWarningMessage('No recommended restaurant. Make sure you have favorites.');
+      } else {
+    	listRestaurants(restaurants);
+      }
+    },
+    // failed callback
+    function () {
+      showErrorMessage('Cannot load recommended restaurants.');
+    } 
+  );
+}
+
+/**
+ * API #4
+ * Toggle favorite (or visited) restaurants
+ * 
+ * @param business_id - The restaurant business id
+ * 
+ * API end point: [POST]/[DELETE] /Dashi/history
+ * request json data: { user_id: 1111, visited: [a_list_of_business_ids] }
+ */
 function changeFavoriteRestaurant(business_id) {
   // Check whether this restaurant has been visited or not
   var li = document.getElementById('restaurant-' + business_id);
@@ -151,21 +246,47 @@ function changeFavoriteRestaurant(business_id) {
   });
   var method = isVisited ? 'post' : 'delete';
 
-  ajax(method, url, req, function (res) {
-    var result = JSON.parse(res);
-    if (result.status === 'OK') {
-      li.dataset.visited = isVisited;
-      favIcon.className = isVisited ? 'fa fa-heart' : 'fa fa-heart-o';
-    }
-  });
+  ajax(method, url, req,
+	// successful callback
+	function (res) {
+	  var result = JSON.parse(res);
+	  if (result.status === 'OK') {
+	    li.dataset.visited = isVisited;
+	    favIcon.className = isVisited ? 'fa fa-heart' : 'fa fa-heart-o';
+	  }
+	}
+  );
+}
+
+// -------------------------------------
+//  Create restaurant list
+// -------------------------------------
+
+/**
+ * List restaurants
+ * 
+ * @param restaurants - An array of restaurant JSON objects
+ */
+function listRestaurants(restaurants) {
+  // Clear the current results
+  var restaurantList = document.getElementsByClassName('restaurant-list')[0];
+  restaurantList.innerHTML = '';
+
+  for (var i = 0; i < restaurants.length; i++) {
+    addRestaurant(restaurantList, restaurants[i]);
+  }
 }
 
 /**
  * Add restaurant to the list
+ * 
+ * @param restaurantList - The <ul class="restaurant-list"> tag
+ * @param restaurant - The restaurant data (JSON object)
  */
 function addRestaurant(restaurantList, restaurant) {
   var business_id = restaurant['business_id'];
   
+  // create the <li> tag and specify the id and class attributes
   var li = $('li', {
 	id: 'restaurant-' + business_id,
     className: 'restaurant'
@@ -175,7 +296,7 @@ function addRestaurant(restaurantList, restaurant) {
   li.dataset.business = business_id;
   li.dataset.visited = restaurant.is_visited;
 
-  // image
+  // restaurant image
   li.appendChild($('img', {src: restaurant.image_url}));
 
   // section
@@ -229,51 +350,4 @@ function addRestaurant(restaurantList, restaurant) {
   restaurantList.appendChild(li);
 }
 
-function $(tag, options) {
-  var element = document.createElement(tag);
-
-  for (var option in options) {
-    if (options.hasOwnProperty(option)) {
-      element[option] = options[option];
-    }
-  }
-
-  return element;
-}
-
-function activeBtn(btnId) {
-  var btns = document.getElementsByClassName('main-nav-btn');
-  for (var i = 0; i < btns.length; i++) {
-    btns[i].className = btns[i].className.replace(/\bactive\b/, '');
-  }
-
-  var btn = document.getElementById(btnId);
-  btn.className = btn.className + ' active';
-}
-
-/**
- * Ajax helper
- */
-function ajax(method, url, data, callback, errorHandler) {
-  var xhr = new XMLHttpRequest();
-
-  xhr.open(method, url, true);
-
-  xhr.onload = function () {
-    if (xhr.status == 200) {
-      callback(xhr.responseText);
-    }
-  };
-
-  xhr.onerror = function () {
-    console.error("The request couldn't be completed.");
-    errorHandler();
-  };
-
-  if (data === null) {
-    xhr.send();
-  } else {
-    xhr.setRequestHeader("Content-Type", "application/json;charset=utf-8");
-    xhr.send(data);
-  }
-}
+// END
