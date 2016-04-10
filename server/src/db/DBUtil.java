8f877d84066d983bf0d4c9aeb1a2b878c54768f6
chenww05 <chenww05@gmail.com>
2016-04-10 15:48:19 -0700

    add mongodb implmentation of verify login

--- server/src/db/DBUtil.java
+++ server/src/db/DBUtil.java
@@ -3,12 +3,11 @@ package db;
 public class DBUtil {
 	private static final String HOSTNAME = "localhost";
 	private static final String PORT_NUM = "3306";
-	public static final String DB_NAME = "laiproject";
 	private static final String USERNAME = "root";
 	private static final String PASSWORD = "root";
-	public static final String MYSQL_URL;
-	static {
-		MYSQL_URL = "jdbc:mysql://" + HOSTNAME + ":" + PORT_NUM + "/" + DB_NAME
-				+ "?user=" + USERNAME + "&password=" + PASSWORD;
-	}
+	
+	public static final String DB_NAME = "laiproject";
+	public static final String MYSQL_URL = "jdbc:mysql://" + HOSTNAME + ":"
+			+ PORT_NUM + "/" + DB_NAME + "?user=" + USERNAME + "&password="
+			+ PASSWORD;
 }
